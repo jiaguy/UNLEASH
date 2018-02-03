78db1e32df7c5ac812542f81ee87e038a55089bc
Celine Zhang <celinezhang_@hotmail.com>
2018-02-03 15:21:25 -0500

    fixing the changes

--- app/src/main/java/ellehacks/unleash/MainActivity.java
+++ app/src/main/java/ellehacks/unleash/MainActivity.java
@@ -13,13 +13,6 @@ import android.widget.ImageView;
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
-import com.spotify.sdk.android.player.Config;
-import com.spotify.sdk.android.player.ConnectionStateCallback;
-import com.spotify.sdk.android.player.Error;
-import com.spotify.sdk.android.player.Player;
-import com.spotify.sdk.android.player.PlayerEvent;
-import com.spotify.sdk.android.player.Spotify;
-import com.spotify.sdk.android.player.SpotifyPlayer;
 
 /**
  * Created by muska on 2018-02-03.
