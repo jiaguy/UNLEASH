fb816bf13190fe2742a3c0b4606b343017a65c09
Jiaying <jiayingli123@gmail.com>
2018-02-03 05:45:13 -0500

    Getting result from textbox

--- app/src/main/java/ellehacks/unleash/EnterFeelings.java
+++ app/src/main/java/ellehacks/unleash/EnterFeelings.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class EnterFeelings extends AppCompatActivity {
 
@@ -28,6 +31,13 @@ public class EnterFeelings extends AppCompatActivity {
                 launchFeelingsResultsActivity();
             }
         });
+
+        EditText edit = (EditText)findViewById(R.id.feelings_textbox);
+        String result = edit.getText().toString();      // result from textbox
+        Log.d("string result", result);
+
+        //TextView tview = (TextView)findViewById(R.id.textview1);
+        //tview.setText(result);
 
         ImageButton home = (ImageButton) findViewById(R.id.home_btn);
         home.setOnClickListener(new View.OnClickListener() {
